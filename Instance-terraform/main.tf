644cb7c67f3fc05cd31d1916557820b2d1acffe1
manishaacharya111 <116179205+manishaacharya111@users.noreply.github.com>
2024-05-24 16:30:23 +0200

    Update main.tf

--- Instance-terraform/main.tf
+++ Instance-terraform/main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "Jenkins-sg" {
 resource "aws_instance" "web" {
   ami                    = "ami-0f58b397bc5c1f2e8"  #change your ami value according to your aws instance
   instance_type          = "t2.large"
-  key_name               = "dec-key"
+  key_name               = "may"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./script.sh", {})
 
@@ -47,7 +47,7 @@ resource "aws_instance" "web" {
 resource "aws_instance" "web2" {
   ami                    = "ami-0f58b397bc5c1f2e8" #change your ami value according to your aws instance 
   instance_type          = "t2.medium"
-  key_name               = "my key"
+  key_name               = "may"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   tags = {
     Name = "Monitering via grafana"
