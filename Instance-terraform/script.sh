5ae0179bf31fde19606dd9947db878575d20a1e8
manishaacharya111 <116179205+manishaacharya111@users.noreply.github.com>
2024-06-19 15:43:03 +0200

    Update script.sh

--- Instance-terraform/script.sh
+++ Instance-terraform/script.sh
@@ -22,7 +22,7 @@ sudo usermod -aG docker jenkins
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
 sudo systemctl restart jenkins
-docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+docker run -d --name sonar -p 9000:9000 sonarqube
 
 # install trivy
 sudo apt-get install wget apt-transport-https gnupg lsb-release -y
